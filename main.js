c5f88598946fe8b7970713c04e95c2cfcb84c3fd
xxnepptoonxx <113563351+xxnepptoonxx@users.noreply.github.com>
2023-06-07 16:56:52 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+function setup() {
+    video= createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas= createCanvas(550, 550);
+    canvas.position(500,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!')
+}
+
+function draw() {
+    background('#086699')
+    textSize(difference)
+    fill('#cdd993');
+    text('Michael', 10, 200);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist;
+        rightWristX = results[0].pose.rightWrist;
+        difference = floor(leftWristX - rightWristX);
+    }
+}
+
